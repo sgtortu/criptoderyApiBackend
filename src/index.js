11cd07d92c0c3882305c7b018cad9ecb8ae12bdd
Santiago Tortu <santiago.tortu@onetree.com>
2023-09-14 12:17:32 -0300

    add dotenv for enviroment variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const v1UserRouter = require('../src/v1/routes/userRoutes');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
