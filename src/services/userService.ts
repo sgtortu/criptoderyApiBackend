cef79acbfcc85de3b7a36bc6900f69b2b51de0b1
sgtortu <sg.tortu@gmail.com>
2023-11-08 18:19:07 -0300

    implement jwt

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const bcrypt = require('bcrypt');
 const {v4: uuid} = require('uuid');
 const prisma = new PrismaClient();
+const jwt = require('jsonwebtoken');
 
 export const getAllUsersService = async () => {
     try {
@@ -31,21 +32,33 @@ export const createNewUserService = async (newUser: any) => {
     if (userExist.length) throw { message: "User Already Exist.", status: 409 };
     
     const encryptedPassword = await bcrypt.hash(newUser.password, 10);
+    const userId = uuid();
+
+    const token = jwt.sign({ 
+            user_id: userId, 
+            email: newUser.email,
+        },
+        process.env.TOKEN_KEY,
+        {
+            expiresIn: "2h",
+        });
     const userToInsert: any = {
         ...newUser,
         password: encryptedPassword,
         id: uuid(),
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
-        isDeleted: false,
+        isDeleted: false,   
         status: 1,
         role: '1',
+        token: token,
     };  
+
     try {
         const createdUser = await prisma.user.create({data: userToInsert});
         return createdUser;
     } catch (error) {
-        return error;
+        throw { error, status: 500 };
     }
 };
 
@@ -59,4 +72,25 @@ export const updateOneUserService = async (userId: string, changes: any) => {
 
 export const deleteOneUserService = async (userId: string) => {
     await prisma.user.delete({ where: { id: userId }});
+};
+
+export const loginService = async (email: string, password: string) => {
+    const user = await prisma.user.findMany({
+        where: { email: email }
+    });
+
+    if (user[0] && (await bcrypt.compare(password, user[0].password))) {
+        const token = jwt.sign(
+          { user_id: user[0].id, email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+  
+        user[0].token = token;
+  
+        return user;
+    }
+    throw { error: "Invalid Credentials", status: 400 };
 };
